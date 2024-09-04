f53d3f5f98a70ea9ce2fe97debfc593a33fee48e
eLaubster <laubeevan@gmail.com>
2024-09-04 18:24:41 -0500

    Allow for setting whether or not the button sets its text to result of its action

--- ui/button.py
+++ ui/button.py
@@ -2,15 +2,16 @@
 from .element import Element
 
 class Button(Element):
-    def __init__(self, label, action, refreshFunction=None, color=-1, background=-1):
+    def __init__(self, label, action, refreshFunction=None, color=-1, background=-1, setLabelToResult=False):
         super().__init__(label, refreshFunction=refreshFunction, color=color, background=background)
         self.action = action
         self.selectable = True
+        self.setLabelToResult = setLabelToResult
 
     def triggerAction(self):
         result = self.action()
-        if result != None:
-            self.label = result
+        if type(result) == str and self.setLabelToResult:
+            self.label = str(result)
 
     def getStr(self, selected=False):
         if selected:
