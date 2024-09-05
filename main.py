53a06d4ba3b985dfb1c2ed23123112549df6ca58
Evan Laube <laubeevan@gmail.com>
2024-09-05 17:10:21 -0500

    Add check for client ID before launching app

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 
 from spotifyApi import SpotifyApi
 from spotifyAppController import SpotifyAppController
+from dotenv import set_key, get_key
 import curses
 
 from ui import UIManager
 
 def main(stdscr):
+
+    clientId = get_key('.env', 'TFY_CLIENT_ID')
+    
+    if clientId == None:
+        print("No Client ID found in .env file.")
+        id = input("\tEnter your Spotify App Client Id: ")
+        set_key('.env', 'TFY_CLIENT_ID', id)
+        curses.wrapper(main)
+        return
+
     api = SpotifyApi()
     uiManager = UIManager(stdscr, api)
     controller = SpotifyAppController(api, uiManager)
@@ -13,4 +24,4 @@ def main(stdscr):
     controller.run()
 
 
-curses.wrapper(main)
+main(None)
