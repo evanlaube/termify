5e82724f17bb29a8a8187d6ae2907c04eefafeac
Evan Laube <laubeevan@gmail.com>
2024-09-05 18:56:28 -0500

    Setup for publishing to PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='termify',
+        name='termify-py',
         version='1.0.0',
         description='A terminal-based Spotify controller',
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        url='https://github.com/evanlaube/termify',
         author='Evan Laube',
         author_email='laubeevan@gmail.com',
+        license='GPT-3.0',
         packages=find_packages(),
         install_requires=[
             'requests',
