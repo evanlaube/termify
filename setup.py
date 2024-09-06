3b322e36e4609332a177be696cf4d0c9162fd334
Evan Laube <laubeevan@gmail.com>
2024-09-05 19:32:29 -0500

    Increment version to v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='termify-py',
-        version='1.0.0',
+        version='1.0.1',
         description='A terminal-based Spotify controller',
         long_description=open('README.md').read(),
         long_description_content_type='text/markdown',
