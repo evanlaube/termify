634102ba6ed6a32ff8d7625905f18477af1ca46a
Evan Laube <laubeevan@gmail.com>
2024-09-07 00:53:50 -0500

    Increment version to v1.1.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
         name='termify-py',
-        version='1.0.1',
+        version='1.1.0',
         description='A terminal-based Spotify controller',
         long_description=open('README.md').read(),
         long_description_content_type='text/markdown',
         url='https://github.com/evanlaube/termify',
         author='Evan Laube',
         author_email='laubeevan@gmail.com',
-        license='GPT-3.0',
+        license='GPL-3.0',
         packages=find_packages(),
         install_requires=[
             'requests',
